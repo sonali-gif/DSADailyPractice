031d60242d37ee5be6c70c81bac17c413cc14efe
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-06-17 19:49:25 +0530

    find missing element in array brute force using linear search

--- Array/findMissingNum.cpp
+++ Array/findMissingNum.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
-//brute force
-int missingNum(vector<int>& arr) {
-    int n = arr.size() + 1;
-
-    // Iterate from 1 to n and check
-    // if the current number is present
-    for (int i = 1; i <= n; i++) {
-        bool found = false;
-        for (int j = 0; j < n - 1; j++) {
-            if (arr[j] == i) {
-                found = true;
+ int findMissingNum(int arr[],int n){
+    for(int i=1;i<=n;i++){
+        int flag=0;
+        for (int j=0;j<n-1;j++){
+            if(arr[j]==i){
+                flag=1;
                 break;
             }
         }
-
-        // If the current number is not present
-        if (!found)
+        if (flag==0){
             return i;
+        }
     }
-    return -1;
-}
-
-int main() {
-    vector<int> arr = {8, 2, 4, 5, 3, 7, 1}; 
-    cout << missingNum(arr) << endl; 
-    return 0;
-}
+   return -1; 
+ }
+ int main(){
+    int n;
+    cout<<"enter size of array ";
+    cin>>n;
+    int arr[n-1];
+    cout<<"elements: ";
+    for (int i=0;i<n-1;i++){
+        cin>>arr[i];
+    }
+    cout<< "missing num "<<findMissingNum(arr,n);
+    
+     return 0;
+ }
