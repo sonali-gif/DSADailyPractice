e3554d080685913b83ccea18c96bd8822c4f7c29
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-06-18 12:13:55 +0530

    Sql quesries practice

--- SQL/sql7.sql
+++ SQL/sql7.sql
@@ -23,3 +23,17 @@ set marks =marks+1;
 delete from student 
 where marks <20;
 
+-- foreign key
+create table t1( -- parent table
+id int primary key,
+name varchar(30)
+);
+
+create table t2(
+id int primary key, -- child table
+name varchar(30),
+dept_id int ,
+foreign key (dept_id)references t1(id)
+on update cascade -- if there is update in parent table auto update in child table
+on delete cascade -- if parent tables id delete it automatically delte it from child table also
+)
---
+++ SQL/sql8.sql
@@ -0,0 +1,98 @@
+use college;
+
+alter table student
+add surname varchar(20);
+select * from student;
+ 
+ -- delete column
+alter table student
+drop surname ;
+
+-- chng col name
+alter table student
+change name f_name varchar(30);
+
+-- modify predefined column datatyp
+alter table student
+modify f_name varchar(40) not null;
+
+-- rename
+alter table student
+rename st;
+
+alter table st
+rename student;
+ -- join operations
+create table s1(
+student_id int primary key,
+name varchar(30)
+);
+ insert into s1 values(101, "adam"),(102,"bob"),(103,"casey");
+create table course(
+student_id int primary key,
+course varchar(30)
+);
+insert into course values(102,"english"),(105,"math"),(103,"science"),(107,"cs");
+select * from course;
+select * from s1;
+-- inner join
+select * from s1 
+inner join course 
+on s1.student_id=course.student_id;
+
+-- left join
+select * from s1 
+left join course 
+on s1.student_id=course.student_id;
+
+-- right join
+select * from s1 
+right join course 
+on s1.student_id=course.student_id;
+
+
+-- full join
+select * from s1 
+left join course 
+on s1.student_id=course.student_id
+union  -- here union will give unique values only
+select * from s1 
+right join course 
+on s1.student_id=course.student_id;
+
+select * from s1 
+left join course 
+on s1.student_id=course.student_id
+union all -- this will also give same values
+select * from s1 
+right join course 
+on s1.student_id=course.student_id;
+
+-- right exclusive
+select * from s1 
+right join course 
+on s1.student_id=course.student_id
+where s1.student_id is null;
+
+-- left exclusive
+select * from s1 
+left join course 
+on s1.student_id=course.student_id
+where course.student_id is null;
+
+select * from s1 
+right join course 
+on s1.student_id=course.student_id
+where s1.student_id is null
+union
+select * from s1 
+left join course 
+on s1.student_id=course.student_id
+where course.student_id is null;
+
+-- self join
+select * from s1 as a
+ join course as b
+on a.student_id=b.student_id;
+
+
