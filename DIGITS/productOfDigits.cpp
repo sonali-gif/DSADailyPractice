a8a4a455d988009963343522d8801fedc89bd4ee
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-12 18:24:56 +0530

    Product of digitd in number

---
+++ DIGITS/productOfDigits.cpp
@@ -0,0 +1,15 @@
+//Product of digits
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int product=1;
+    while(n>0){
+        int digit=n%10;
+        product *=digit;
+        n=n/10;
+    }
+    cout<<product;
+    return 0;
+}
