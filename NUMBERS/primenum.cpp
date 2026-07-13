4fdf0b016d7a67d10e69d518ffb71a5aa4009d19
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-13 21:11:44 +0530

    Prime Number

---
+++ NUMBERS/primenum.cpp
@@ -0,0 +1,20 @@
+//prime number
+#include <bits/stdc++.h>
+using namespace std;
+bool isprime(int n){
+    //time comp-O(n)
+    if(n<2){
+        return false;
+    }
+    for(int i=2;i*i<=n;i++){ 
+    if(n%i==0){
+        return false;
+    }}
+    return true;
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<isprime(n);
+    return 0;
+}
