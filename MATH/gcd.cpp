907c9dacddb83895f6e05f2d59ed675fa1de3d62
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-14 09:00:38 +0530

    GCD of 2 numbers

---
+++ MATH/gcd.cpp
@@ -0,0 +1,18 @@
+//GCD (Euclidean) GCD(a,b)=GCD(b,a%b)
+//Find GCD of 48 and 18
+// 48 % 18 = 12
+// 18 % 12 = 6
+// 12 % 6  = 0 GCD=6
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    while(b>0){
+       int rem= a%b;
+       a=b;
+       b=rem;
+    }
+    cout<<"GCD= "<<a;
+    return 0;
+}
