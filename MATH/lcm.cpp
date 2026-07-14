6a4ac554cd8bb3ae9b715b362527d34534773bdb
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-14 09:14:46 +0530

    LCM of 2 numbers

---
+++ MATH/lcm.cpp
@@ -0,0 +1,15 @@
+//LCM
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,b;
+    cin >>a>>b;
+    int lcm=max(a,b);
+    while(true){
+        if(lcm%a==0&&lcm %b==0) break;
+    
+    lcm++;
+}
+cout<<"LCM: "<<lcm;
+return 0;
+}
