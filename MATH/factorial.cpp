8077063f54291a26e8eda7e14e3a00ffa7e37605
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-14 09:21:46 +0530

    Factorial

---
+++ MATH/factorial.cpp
@@ -0,0 +1,13 @@
+//Factorial
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int fact=1;
+    for(int i=1;i<=a;i++){
+        fact *=i;
+    }
+    cout<<fact;
+    return 0;
+}
