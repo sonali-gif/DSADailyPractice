0590d752275cc8fa3d519069245e9e6627011631
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-07-16 09:32:09 +0530

    Push Zeros to End — Chocolate Factory prblm

---
+++ ARRAYY/push0toend.cpp
@@ -0,0 +1,34 @@
+//A chocolate factory is packing chocolates into packets. The chocolate packets are represented as an array of N integer values.
+// The task is to find the empty packets (represented as 0) and push them to the end of the conveyor belt (array), 
+//while preserving the relative order of the non-zero elements.
+// Input Format First line: an integer N — the number of packets.
+// Second line: N space-separated integers denoting the number of chocolates in each packet (0 = empty packet)
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> movezero(vector<int>v ,int n){
+    //time comp,space comp-O(N)
+    vector<int>ans;
+    for(int i=0;i<n;i++){
+        if(v[i]!=0){
+            ans.push_back(v[i]);
+        }
+    }
+    int rem=n-ans.size();
+    for(int i=0;i<rem;i++){
+      ans.push_back(0);
+    }
+    return ans;
+}
+int main(){
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    vector<int>ans=movezero(v,n);
+    for(auto it:ans){
+        cout<<it<<" ";
+    }
+    return 0;
+}
---
+++ ARRAYY/tempCodeRunnerFile.cpp
@@ -0,0 +1,4 @@
+ // int rem=n-ans.size();
+    // for(int i=rem;i<n;i++){
+    //     cout<<0;
+    // }
