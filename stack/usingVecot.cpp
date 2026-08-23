1fc5755889e6106e966efbd0a59c737f11fc5c73
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-08-23 15:53:00 +0530

    Stack using vector

---
+++ stack/usingVecot.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class stackk{
+    public:
+    vector<int>v;
+    
+    void push(int val){
+        v.push_back(val);
+    }
+    void pop(){
+        v.pop_back();
+    }
+    int top(){
+        return v[v.size()-1];
+    }
+    bool empty(){
+        return v.size()==0;
+    }
+};
+int main(){
+    stackk s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    while(!s.empty()){
+        cout<<s.top()<<" ";
+        s.pop();
+    }
+    cout<<endl;
+    return 0;
+}
