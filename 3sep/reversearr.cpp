92b4500c00563652d6fe004cfd87d25292630bc0
Sonali Agnihotri <sonaliagnihotri2416@gmail.com>
2026-09-03 17:25:47 +0530

    reverse array

---
+++ 3sep/reversearr.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int low=0;
+    int high=n-1;
+    while(low<high){
+        swap(arr[low],arr[high]);
+        low++;
+        high--;
+    }
+    cout<<"reverse: "<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
